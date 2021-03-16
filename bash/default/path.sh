6294b80c7ec8c41ecb29a19a8b11bef71d52b22c
Erin Sheldon <erin.sheldon@gmail.com>
2021-03-15 23:02:53 -0400

    only modify if exists

--- bash/default/path.sh
+++ bash/default/path.sh
@@ -18,7 +18,8 @@ if [[ -d ${HOME}/scripts ]]; then
     prepend_path PATH $HOME/scripts
 fi
 
+ANACONDA_DIR=$HOME/miniconda3
 if [[ -e $ANACONDA_DIR ]]; then
-    export ANACONDA_DIR=$HOME/miniconda3
+    export ANACONDA_DIR
     prepend_path PATH ${ANACONDA_DIR}/bin
 fi
