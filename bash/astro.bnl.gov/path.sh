cb3901343499d87ef4428c3cfe9c6bc121735dda
Erin Sheldon <erin.sheldon@gmail.com>
2023-05-18 15:09:18 -0400

    support mambaforge

--- bash/astro.bnl.gov/path.sh
+++ bash/astro.bnl.gov/path.sh
@@ -2,8 +2,9 @@ append_path PATH /sbin
 append_path PATH /usr/sbin
 
 prepend_path PATH /opt/astro/SL64/texlive/2013/bin/x86_64-linux/
+prepend_path PATH $HOME/wq.dist/
 
-ANACONDA_DIR=/gpfs02/astro/desdata/esheldon/miniconda3
+ANACONDA_DIR=/gpfs02/astro/desdata/esheldon/mambaforge
 if [[ -e $ANACONDA_DIR ]]; then
     export ANACONDA_DIR
     prepend_path PATH ${ANACONDA_DIR}/bin
