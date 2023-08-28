8a5bf6035db7a5fdaa4fa531c99bbdfcd2a9940f
Erin Sheldon <esheldon@sdfrome002.sdf.slac.stanford.edu>
2023-08-28 10:54:52 -0700

    setups for usdf

--- bash/usdf/localvars.sh
+++ bash/usdf/localvars.sh
@@ -1,1 +1,6 @@
 export TMPDIR="/run/user/$(id -u)"
+
+# SLAC S3DF - source all files under ~/.profile.d
+if [[ -e ~/.profile.d && -n "$(ls -A ~/.profile.d/)" ]]; then
+  source <(cat $(find -L  ~/.profile.d -name '*.conf'))
+fi
