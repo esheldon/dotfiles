b1a1104c9403228531a95bbb0a2c6007365bf891
Erin Sheldon <erin.sheldon@gmail.com>
2024-01-22 22:14:13 -0500

    add thorin

---
+++ bash/thorin/localvars.sh
@@ -0,0 +1,13 @@
+export TMPDIR=/home/esheldon/data/tmp
+
+export LENSDIR=$HOME/lensing
+
+export DESDATA=$HOME/data/DES
+export DESREMOTE=https://desar2.cosmology.illinois.edu/DESFiles/desardata
+export DESREMOTE_RSYNC=desar2.cosmology.illinois.edu::ALLDESFiles/desarchive
+export DES_RSYNC_PASSFILE=$HOME/.des_rsync_pass
+export DESPROJ=OPS
+
+export MEDS_DIR=$HOME/data/DES/meds
+
+export CATSIM_DIR=$HOME/data/catsim
