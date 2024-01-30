9ebcc1a2f2c3fafb90195f1feed0324b4e10963f
Erin Sheldon <erin.sheldon@gmail.com>
2024-01-30 15:16:10 -0500

    numlock off

--- bash/thorin/localvars.sh
+++ bash/thorin/localvars.sh
@@ -11,3 +11,6 @@ export DESPROJ=OPS
 export MEDS_DIR=$HOME/data/DES/meds
 
 export CATSIM_DIR=$HOME/data/catsim
+
+export RUBIN_SIM_DATA_DIR=/home/esheldon/data/rubin_sim_data
+export SIMS_SED_LIBRARY_DIR=$RUBIN_SIM_DATA_DIR/sims_sed_library
