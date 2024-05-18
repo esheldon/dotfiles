bf55caddc790908368882f75c0ea1b25a9a77b89
Erin Sheldon <erin.sheldon@gmail.com>
2024-05-17 20:31:34 -0400

    new files

---
+++ bash/piano/localvars.sh
@@ -0,0 +1,5 @@
+export TMPDIR=/tmp
+export CATSIM_DIR=$HOME/data/catsim
+export IMSIM_DIR=$HOME/git
+export RUBIN_SIM_DATA_DIR=/home/esheldon/data/rubin_sim_data
+export SIMS_SED_LIBRARY_DIR=$RUBIN_SIM_DATA_DIR/sims_sed_library
