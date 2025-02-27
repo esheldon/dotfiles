ce64230798e9d34a94b2128e7789a954eeac1de5
Erin Sheldon <erin.sheldon@gmail.com>
2025-02-27 11:32:13 -0500

    use new tex

--- bash/astro.bnl.gov/path.sh
+++ bash/astro.bnl.gov/path.sh
@@ -1,11 +1,6 @@
 append_path PATH /sbin
 append_path PATH /usr/sbin
 
-prepend_path PATH /opt/astro/SL64/texlive/2013/bin/x86_64-linux/
-prepend_path PATH $HOME/wq.dist/
+prepend_path PATH /cvmfs/astro.sdcc.bnl.gov/SL73/packages/texlive/2024/bin/x86_64-linux
 
-ANACONDA_DIR=/gpfs02/astro/desdata/esheldon/mambaforge
-if [[ -e $ANACONDA_DIR ]]; then
-    export ANACONDA_DIR
-    prepend_path PATH ${ANACONDA_DIR}/bin
-fi
+prepend_path PATH $HOME/wq.dist/
