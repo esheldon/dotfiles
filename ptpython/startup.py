abdae66a88129a6966c612015de524ae349c1d01
Erin Sheldon <erin.sheldon@gmail.com>
2025-05-16 12:12:25 -0400

    guard imports

--- ptpython/startup.py
+++ ptpython/startup.py
@@ -1,17 +1,42 @@
 from glob import glob
-import ngmix
-import numpy as np
-from numpy import (
-    array,zeros,ones,where,arange,linspace,logspace,
-    sqrt, exp, cos, sin, tanh, arctanh, log, log10, median,
-    diag
-)
-import esutil as eu
-from esutil.numpy_util import ahelp, aprint, where1, between
-from esutil.misc import colprint, ptime
-from esutil.stat import get_stats, print_stats
-import espy.plotting
-from espy.plotting import plot
-from espy.plotting import plot_hist
-import fitsio
-import matplotlib.pyplot as mplt
+
+try:
+    import numpy as np
+    from numpy import (
+        array,zeros,ones,where,arange,linspace,logspace,
+        sqrt, exp, cos, sin, tanh, arctanh, log, log10, median,
+        diag
+    )
+except ModuleNotFoundError:
+    print('could not import numpy')
+
+try:
+    import ngmix
+except ModuleNotFoundError:
+    print('could not import ngmix')
+
+try:
+    import esutil as eu
+    from esutil.numpy_util import ahelp, aprint, where1, between
+    from esutil.misc import colprint, ptime
+    from esutil.stat import get_stats, print_stats
+except ModuleNotFoundError:
+    print('could not import esutil')
+
+try:
+    import espy.plotting
+    from espy.plotting import plot
+    from espy.plotting import plot_hist
+except ModuleNotFoundError:
+    print('could not import espy')
+
+try:
+    import fitsio
+except ModuleNotFoundError:
+    print('could not import fitsio')
+
+try:
+    import matplotlib
+    import matplotlib.pyplot as mplt
+except ModuleNotFoundError:
+    print('could not import matplotlib')
